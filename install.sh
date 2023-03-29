a8efb9c5435119dd7edf8878ab98fa851298b45d
matheus-1618 <matheus1207live@gmail.com>
2023-03-29 11:28:06 -0300

    Update dependencies of roadmap

--- install.sh
+++ install.sh
@@ -17,12 +17,25 @@ sudo apt update
 
 sudo apt-get install terraform
 
+#Instalando Node-js versão adequada para React
+curl -sL https://deb.nodesource.com/setup_14.9 -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs
 
+#Instalando Python e pip
+sudo apt-get install python3
+sudo apt-get install python3-pip
+ 
 #Criando diretórios para utilização
 mkdir lambda_functions
 mkdir python
 mkdir terraform
+mkdir webapp
 
 # Configurando depedências da automação via Python
-cd python/
 python3 -m virtualenv venv
+pip install -r requirements.txt
+
+# Criando aplicativo react 
+cd webapp/
+npx create-react-app Lusk-app
